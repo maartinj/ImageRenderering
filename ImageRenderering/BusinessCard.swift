1132a5e42fb83ffbec4035f865e4c20201f39eb8
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-10-31 01:07:12 +0100

    feat: Example 1

--- ImageRenderering/BusinessCard.swift
+++ ImageRenderering/BusinessCard.swift
@@ -8,27 +8,84 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct BusinessCard: View {
+    @StateObject var imagePicker = ImagePicker()
     @State private var renderedImage: Image?
+    @State private var name = ""
+    @State private var company = ""
+    @State private var email = ""
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack {
-                    // Businesss Card Entry and Display
+                    VStack {
+                        TextField("Name", text: $name)
+                        TextField("Company", text: $company)
+                        TextField("Email Address", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textInputAutocapitalization(.never)
+                    }
+                    .textFieldStyle(.roundedBorder)
+                    cardView
                     Divider()
                     Spacer()
                     VStack {
                         Button("Render Image") {
+                            let rendered = ImageRenderer(content: cardView)
+                            rendered.scale = 3
+                            if let image = rendered.cgImage {
+                                renderedImage = Image(decorative: image, scale: 1.0)
+                            }
                         }
                         .buttonStyle(.borderedProminent)
                         RenderedImageView(renderedImage: $renderedImage)
                             .navigationTitle("Business Card")
+                        if let renderedImage {
+                            ShareLink(item: renderedImage, preview: SharePreview("My card", image: renderedImage))
+                        }
                     }
                 }
                 .padding()
             }
         }
+    }
+    var cardView: some View {
+        HStack {
+            PhotosPicker(selection: $imagePicker.imageSelection, matching: .images, photoLibrary: .shared()) {
+                if let image = imagePicker.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 75, height: 75)
+                        .clipShape(Circle())
+                } else {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 75, height: 75)
+                }
+            }
+
+            VStack(alignment: .leading, spacing: 15) {
+                Text(name)
+                    .font(.title3.bold())
+                Text(company)
+                Text(email)
+            }
+            Spacer()
+        }
+        .padding()
+        .frame(width: 325, height: 150)
+//        .background(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1))
+        .background(RoundedRectangle(cornerRadius: 10)
+            .fill(Color(.systemBackground))
+            .overlay {
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 1)
+            }
+        )
     }
 }
 
